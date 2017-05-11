788c8d37789d7001957d91841b587c7a9a31e6ba
user <dev@stephentung.net>
2017-05-11 16:54:44 -0700

    updated browser-roller

--- browser-roller/node-scripts/loadD3Info.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-var infoLoader = require("../js/d3InfoLoader.js");
-infoLoader.loadModulesInfo({"d3-scale-chromatic": "latest"}, function(error, info){
-	fs.writeFile(__dirname+"/../data/d3Info.json", JSON.stringify(info, null, "  "));
-});
